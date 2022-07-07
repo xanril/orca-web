b82c3b722120e6ad814a9d3a53bb3126c404598f
Kristian Mitra <xanril@users.noreply.github.com>
2022-07-07 18:40:32 +0800

    initial files for manage-add-movie component

--- projects/app/src/app/app.module.ts
+++ projects/app/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ManageMovieComponent } from './manage-movie/manage-movie.component';
+import { ManageAddMovieComponent } from './manage-movie/manage-add-movie/manage-add-movie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ManageMovieComponent,
+    ManageAddMovieComponent
   ],
   imports: [
     BrowserModule
---
+++ projects/app/src/app/manage-movie/manage-add-movie/manage-add-movie.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-add-movie',
+  templateUrl: './manage-add-movie.component.html',
+  styleUrls: ['./manage-add-movie.component.css']
+})
+export class ManageAddMovieComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

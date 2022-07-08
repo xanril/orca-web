5e268f38e1551345c2a7ed06a4eeac3cc7c03726
Kristian Mitra <xanril@users.noreply.github.com>
2022-07-08 17:17:25 +0800

    move shoelace initialiation away from index.html

--- projects/app/src/main.ts
+++ projects/app/src/main.ts
@@ -4,6 +4,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { setBasePath } from '@shoelace-style/shoelace';
+
+// set base assets path for shoelace-style components
+setBasePath("/assets/shoelace");
+
 if (environment.production) {
   enableProdMode();
 }

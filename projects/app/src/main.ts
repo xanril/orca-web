5255a5144cd9bf810de5a5b99b158631271c857f
Kristian Mitra <xanril@users.noreply.github.com>
2022-07-09 06:58:35 +0800

    remove shoelace-style components integration

--- projects/app/src/main.ts
+++ projects/app/src/main.ts
@@ -4,11 +4,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import { setBasePath } from '@shoelace-style/shoelace';
-
-// set base assets path for shoelace-style components
-setBasePath("/assets/shoelace");
-
 if (environment.production) {
   enableProdMode();
 }

66a9d4eb83b5557e6080a3b9d76f3491fba6d332
Kristian Mitra <xanril@users.noreply.github.com>
2022-07-23 16:27:36 +0800

    initial display ov movies and routerLink functionality

--- projects/app/src/app/manage-movie/manage-movie.component.ts
+++ projects/app/src/app/manage-movie/manage-movie.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Movie } from '../models/movie.model';
+import { AppState } from '../store';
 
 @Component({
   selector: 'app-manage-movie',
@@ -7,10 +10,16 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./manage-movie.component.css']
 })
 export class ManageMovieComponent implements OnInit {
+  movies: Movie[] = []
 
-  constructor(private router:Router, private activatedRoute:ActivatedRoute) { }
+  constructor(private router:Router, 
+    private activatedRoute:ActivatedRoute,
+    private store: Store<AppState>) { }
 
   ngOnInit(): void {
+    this.store.select('movies').subscribe((data) => {
+      this.movies = data.movies;
+    });
   }
 
   onSearch() {
--- projects/app/src/app/navbar/navbar.module.ts
+++ projects/app/src/app/navbar/navbar.module.ts
@@ -2,10 +2,11 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar.component';
 import { NavbarMenuItemComponent } from './navbar-menu-item/navbar-menu-item.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [NavbarComponent, NavbarMenuItemComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [NavbarComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })

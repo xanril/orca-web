901fbaa8b925a01a4f84407754694fb6e0f7c556
Kristian Mitra <xanril@users.noreply.github.com>
2022-07-31 12:36:30 +0800

    implemented theme toggle button

--- projects/app/src/app/navbar/navbar-theme-toggle/navbar-theme-toggle.component.ts
+++ projects/app/src/app/navbar/navbar-theme-toggle/navbar-theme-toggle.component.ts
@@ -6,8 +6,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarThemeToggleComponent implements OnInit {
   isToggledOn: boolean = true;
+  activeTheme: string = 'light';
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const storedTheme = localStorage.getItem('gala-theme');
+    
+    if (!storedTheme) {
+      localStorage.setItem('gala-theme', this.activeTheme);
+      this.isToggledOn = true;
+      this.setTheme('light');
+      return;
+    }
+
+    this.isToggledOn = storedTheme === 'light';
+    this.setTheme(storedTheme);
+  }
+
+  toggleHandler(event: Event) {
+    const checkbox = event.currentTarget as HTMLInputElement;
+    this.isToggledOn = checkbox.checked;
+    this.setTheme(this.isToggledOn ? 'light' : 'dark');
+  }
+
+  setTheme(theme: string) {
+    document.documentElement.setAttribute('data-theme', theme);
+  }
 }

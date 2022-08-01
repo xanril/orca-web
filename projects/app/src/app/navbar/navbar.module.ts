38a40d88eb32589d7ec304be535d0b9b5b7cb428
Kristian Mitra <xanril@users.noreply.github.com>
2022-08-01 13:08:46 +0800

    navbar structure

--- projects/app/src/app/navbar/navbar.module.ts
+++ projects/app/src/app/navbar/navbar.module.ts
@@ -1,12 +1,11 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar.component';
-import { NavbarMenuItemComponent } from './navbar-menu-item/navbar-menu-item.component';
 import { RouterModule } from '@angular/router';
 import { NavbarThemeToggleComponent } from './navbar-theme-toggle/navbar-theme-toggle.component';
 
 @NgModule({
-  declarations: [NavbarComponent, NavbarMenuItemComponent, NavbarThemeToggleComponent],
+  declarations: [NavbarComponent, NavbarThemeToggleComponent],
   imports: [CommonModule, RouterModule],
   exports: [NavbarComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],

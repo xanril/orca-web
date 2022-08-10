6df6cdcafc2bf1094ffab651673a59bfa0e4312f
Kristian Mitra <xanril@users.noreply.github.com>
2022-08-10 15:46:08 +0800

    removed missing imports

--- projects/app/src/app/cinemas/cinemas-routing.module.ts
+++ projects/app/src/app/cinemas/cinemas-routing.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AddCinemaRoomScheduleComponent } from './add-cinema-room-schedule/add-cinema-room-schedule.component';
 import { CinemaDetailComponent } from './cinema-detail/cinema-detail.component';
-import { CinemaRoomScheduleItemComponent } from './cinema-room-schedule-item/cinema-room-schedule-item.component';
 import { CinemasPageComponent } from './cinemas-page/cinemas-page.component';
 import { CinemasStartComponent } from './cinemas-start/cinemas-start.component';
-import { EditCinemaRoomComponent } from './edit-cinema-room/edit-cinema-room.component';
-import { EditCinemaComponent } from './edit-cinema/edit-cinema.component';
 
 const cinemasRoutes: Routes = [
   {

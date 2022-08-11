5ba544b86520850656a5e5fe3be51b60bdf2eea9
Kristian Mitra <xanril@users.noreply.github.com>
2022-08-11 08:19:28 +0800

    cleanup of cinemas module

--- projects/app/src/app/cinemas/cinemas.module.ts
+++ projects/app/src/app/cinemas/cinemas.module.ts
@@ -1,27 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CinemasPageComponent } from './cinemas-page/cinemas-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
 import { CinemasPageRoutingModule } from './cinemas-routing.module';
 import { SharedModule } from '../shared/shared-ui.module';
+import { CinemasPageComponent } from './cinemas-page/cinemas-page.component';
 import { RoomItemComponent } from './components/room-item/room-item.component';
 import { AddNewItemComponent } from './components/add-new-item/add-room-item.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { CinemaRoomScheduleItemComponent } from './cinema-room-schedule-item/cinema-room-schedule-item.component';
-import { AddCinemaRoomScheduleComponent } from './add-cinema-room-schedule/add-cinema-room-schedule.component';
 import { CinemaListComponent } from './components/cinema-list/cinema-list.component';
 import { CinemasStartComponent } from './cinemas-start/cinemas-start.component';
-import { StoreModule } from '@ngrx/store';
-import * as fromCinemasPage from './store/cinemas-page.reducer';
 import { CinemaDetailComponent } from './cinema-detail/cinema-detail.component';
 import { CinemaEditFormComponent } from './components/cinema-edit-form/cinema-edit-form.component';
+import * as fromCinemasPage from './store/cinemas-page.reducer';
 
 @NgModule({
   declarations: [
     CinemasPageComponent,
     RoomItemComponent,
     AddNewItemComponent,
-    CinemaRoomScheduleItemComponent,
-    AddCinemaRoomScheduleComponent,
     CinemaListComponent,
     CinemasStartComponent,
     CinemaDetailComponent,
--- projects/app/src/app/schedules/schedules.module.ts
+++ projects/app/src/app/schedules/schedules.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { SchedulesPageComponent } from './schedules-page/schedules-page.component';
 import { SchedulesStartComponent } from './schedules-start/schedules-start.component';
 import { SchedulesRoutingModule } from './schedules-routing.module';
-
-
+import { AddCinemaRoomScheduleComponent } from './add-cinema-room-schedule/add-cinema-room-schedule.component';
+import { CinemaRoomScheduleItemComponent } from './cinema-room-schedule-item/cinema-room-schedule-item.component';
 
 @NgModule({
   declarations: [
     SchedulesPageComponent,
-    SchedulesStartComponent
+    SchedulesStartComponent,
+    AddCinemaRoomScheduleComponent,
+    CinemaRoomScheduleItemComponent,
   ],
-  imports: [
-    CommonModule,
-    SchedulesRoutingModule
-  ]
+  imports: [CommonModule, ReactiveFormsModule, SchedulesRoutingModule],
 })
-export class SchedulesModule { }
+export class SchedulesModule {}

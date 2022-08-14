caa9ae4943a448cbf259a32938c4821745c9c35d
Kristian Mitra <xanril@users.noreply.github.com>
2022-08-14 17:59:43 +0800

    remove unused components

--- projects/app/src/app/schedules/schedules.module.ts
+++ projects/app/src/app/schedules/schedules.module.ts
@@ -6,8 +6,6 @@ import { SharedModule } from '../shared/shared.module';
 import { SchedulesRoutingModule } from './schedules-routing.module';
 import { SchedulesPageComponent } from './schedules-page/schedules-page.component';
 import { SchedulesStartComponent } from './schedules-start/schedules-start.component';
-import { AddCinemaRoomScheduleComponent } from './add-cinema-room-schedule/add-cinema-room-schedule.component';
-import { CinemaRoomScheduleItemComponent } from './cinema-room-schedule-item/cinema-room-schedule-item.component';
 import { SchedulesSideComponent } from './components/schedules-side/schedules-side.component';
 import { schedulesPageFeature } from './store/schedules-page.reducer';
 import { ScheduleDetailComponent } from './schedule-detail/schedule-detail.component';
@@ -19,8 +17,6 @@ import { AddScheduleItemComponent } from './components/add-schedule-item/add-sch
   declarations: [
     SchedulesPageComponent,
     SchedulesStartComponent,
-    AddCinemaRoomScheduleComponent,
-    CinemaRoomScheduleItemComponent,
     SchedulesSideComponent,
     ScheduleDetailComponent,
     ScheduleItemComponent,

2fdb1aa3cf14aeda756f9f3b6eadd54d615dbc54
Kristian Mitra <xanril@users.noreply.github.com>
2022-08-23 06:40:57 +0800

    initial test of api integration when loading movies

--- projects/app/src/environments/environment.prod.ts
+++ projects/app/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  tmdbApiKey: 'bdba455e0d926241736ae6ddc8ba3681'
+  tmdbApiKey: 'bdba455e0d926241736ae6ddc8ba3681',
+  apiBaseUrl: 'https://orca-api.azurewebsites.net'
 };
--- projects/app/src/environments/environment.ts
+++ projects/app/src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  tmdbApiKey: 'bdba455e0d926241736ae6ddc8ba3681'
+  tmdbApiKey: 'bdba455e0d926241736ae6ddc8ba3681',
+  apiBaseUrl: 'https://orca-api.azurewebsites.net'
 };
 
 /*
